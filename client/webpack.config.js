1f6b701880aa2cfcd2afe5103f6f8f3b3056c94e
Jbeach32 <joshbeach2007@gmail.com>
2023-11-15 14:56:04 -0700

    added plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,18 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'J.A.T.E',
+        short_name: 'J.A.T.E',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
